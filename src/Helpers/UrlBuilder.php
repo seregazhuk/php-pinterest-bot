7d5ae76698a94c4d58762d6fe756e8c701c39016
Zhuk Sergey <seregazhuk88@gmail.com>
2017-02-21 22:25:48 +0300

    fix: UrlBuilder get resources

--- src/Helpers/UrlBuilder.php
+++ src/Helpers/UrlBuilder.php
@@ -22,7 +22,7 @@ class UrlBuilder
      * Boards
      */
     const RESOURCE_GET_BOARDS = 'resource/BoardsResource/get/';
-    const RESOURCE_GET_BOARD = 'resource/BoardResource/execGet';
+    const RESOURCE_GET_BOARD = 'resource/BoardResource/get';
     const RESOURCE_GET_BOARD_FEED = 'resource/BoardFeedResource/get/';
     const RESOURCE_PROFILE_BOARDS = 'resource/ProfileBoardsResource/get/';
     const RESOURCE_FOLLOW_BOARD = 'resource/BoardFollowResource/create/';
@@ -32,7 +32,7 @@ class UrlBuilder
     const RESOURCE_UPDATE_BOARD = 'resource/BoardResource/update/';
     const RESOURCE_BOARD_FOLLOWERS = 'resource/BoardFollowersResource/get/';
     const RESOURCE_FOLLOWING_BOARDS = 'resource/BoardFollowingResource/get/';
-    const RESOURCE_TITLE_SUGGESTIONS = 'resource/BoardTitleSuggestionsResource/execGet';
+    const RESOURCE_TITLE_SUGGESTIONS = 'resource/BoardTitleSuggestionsResource/get';
 
     /**
      * Pins
@@ -47,11 +47,11 @@ class UrlBuilder
     const RESOURCE_COMMENT_PIN = 'resource/PinCommentResource/create/';
     const RESOURCE_COMMENT_DELETE_PIN = 'resource/PinCommentResource/delete/';
     const RESOURCE_PIN_INFO = 'resource/PinResource/get/';
-    const RESOURCE_DOMAIN_FEED = 'resource/DomainFeedResource/execGet';
-    const RESOURCE_ACTIVITY = 'resource/AggregatedActivityFeedResource/execGet';
+    const RESOURCE_DOMAIN_FEED = 'resource/DomainFeedResource/get';
+    const RESOURCE_ACTIVITY = 'resource/AggregatedActivityFeedResource/get';
     const RESOURCE_USER_FEED = 'resource/UserHomefeedResource/get/';
-    const RESOURCE_RELATED_PINS = 'resource/RelatedPinFeedResource/execGet';
-    const RESOURCE_VISUAL_SIMILAR_PINS = 'resource/VisualLiveSearchResource/execGet';
+    const RESOURCE_RELATED_PINS = 'resource/RelatedPinFeedResource/get';
+    const RESOURCE_VISUAL_SIMILAR_PINS = 'resource/VisualLiveSearchResource/get';
     const RESOURCE_BULK_COPY = 'resource/BulkEditResource/create/';
     const RESOURCE_BULK_MOVE = 'resource/BulkEditResource/update/';
     const RESOURCE_BULK_DELETE = 'resource/BulkEditResource/delete/';
@@ -107,7 +107,7 @@ class UrlBuilder
      * Registration
      */
     const RESOURCE_CREATE_REGISTER = 'resource/UserRegisterResource/create/';
-    const RESOURCE_CHECK_EMAIL = 'resource/EmailExistsResource/execGet';
+    const RESOURCE_CHECK_EMAIL = 'resource/EmailExistsResource/get';
     const RESOURCE_SET_ORIENTATION = 'resource/OrientationContextResource/create/';
     const RESOURCE_UPDATE_REGISTRATION_TRACK = 'resource/UserRegisterTrackActionResource/update/';
     const RESOURCE_REGISTRATION_COMPLETE = 'resource/UserExperienceCompletedResource/update/';
@@ -124,13 +124,13 @@ class UrlBuilder
     const RESOURCE_GET_CATEGORIES = 'resource/CategoriesResource/get/';
     const RESOURCE_GET_CATEGORY = 'resource/CategoryResource/get/';
     const RESOURCE_GET_CATEGORIES_RELATED = 'resource/RelatedInterestsResource/get/';
-    const RESOURCE_GET_CATEGORY_FEED = 'resource/CategoryFeedResource/execGet';
+    const RESOURCE_GET_CATEGORY_FEED = 'resource/CategoryFeedResource/get';
 
     /**
      * Topics
      */
-    const RESOURCE_GET_TOPIC_FEED = 'resource/TopicFeedResource/execGet';
-    const RESOURCE_GET_TOPIC = 'resource/TopicResource/execGet';
+    const RESOURCE_GET_TOPIC_FEED = 'resource/TopicFeedResource/get';
+    const RESOURCE_GET_TOPIC = 'resource/TopicResource/get';
 
     /**
      * Invite
