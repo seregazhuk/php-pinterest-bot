8a98738176319df4831a3a4b1d72ea0fe7f61031
Zhuk Sergey <seregazhuk88@gmail.com>
2017-03-16 21:04:49 +0300

    fix: Pagination

--- src/Helpers/Pagination.php
+++ src/Helpers/Pagination.php
@@ -166,6 +166,6 @@ class Pagination implements IteratorAggregate
     {
         $results = $response->getResponseData();
 
-        return $results ? : [];
+        return is_array($results) ? : [];
     }
 }
