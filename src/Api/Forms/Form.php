d0ed8875b094e17ea25541f14d53d90508b2352f
Zhuk Sergey <seregazhuk88@gmail.com>
2017-07-09 13:37:30 +0300

    upd: forms refactoring

--- src/Api/Forms/Form.php
+++ src/Api/Forms/Form.php
@@ -4,8 +4,14 @@ namespace seregazhuk\PinterestBot\Api\Forms;
 
 abstract class Form
 {
+    /**
+     * @return array
+     */
     abstract protected function getData();
 
+    /**
+     * @return array
+     */
     public function toArray(){
         return array_filter($this->getData(), function($item){
             return !is_null($item);
