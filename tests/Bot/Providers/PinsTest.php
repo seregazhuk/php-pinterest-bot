5c4e3a4589e4f089e5d83b020e67ee49561200a8
Zhuk Sergey <seregazhuk88@gmail.com>
2017-08-07 21:04:52 +0300

    pins tryit tests extracted to their own test

--- tests/Bot/Providers/PinsTest.php
+++ tests/Bot/Providers/PinsTest.php
@@ -42,49 +42,6 @@ class PinsTest extends ProviderBaseTest
         $this->assertWasGetRequest(UrlBuilder::RESOURCE_PIN_INFO, $request);
     }
 
-    /** @test */
-    public function a_user_can_try_a_pin()
-    {
-        $provider = $this->getProvider();
-        $provider->tryIt('12345', 'my comment');
-
-        $request = [
-            'pin_id'  => '12345',
-            'details' => 'my comment',
-        ];
-        $this->assertWasPostRequest(UrlBuilder::RESOURCE_TRY_PIN_CREATE, $request);
-    }
-
-    /** @test */
-    public function a_user_can_edit_try_it_of_the_pin()
-    {
-        $provider = $this->getProvider();
-        $provider->editTryIt('12345', '56789', 'my comment');
-
-        $request = [
-            'pin_id'              => '12345',
-            'details'             => 'my comment',
-            'user_did_it_data_id' => '56789',
-        ];
-        $this->assertWasPostRequest(UrlBuilder::RESOURCE_TRY_PIN_EDIT, $request);
-    }
-
-    /** @test */
-    public function it_can_fetch_users_who_have_tried_a_pin()
-    {
-        $provider = $this->getProvider();
-        $this->pinterestShouldReturn(['aggregated_pin_data' => ['id' => '56789']]);
-
-        $provider->tried('12345')->toArray();
-
-        $request = [
-            'field_set_key'    => 'did_it',
-            'show_did_it_feed' => true,
-            'aggregated_pin_data_id' => '56789'
-        ];
-        $this->assertWasGetRequest(UrlBuilder::RESOURCE_ACTIVITY, $request);
-    }
-
     /**
      * @return string
      */
---
+++ tests/Bot/Providers/TryItTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace seregazhuk\tests\Bot\Providers;
+
+use seregazhuk\PinterestBot\Helpers\UrlBuilder;
+
+class TryItTest extends PinsTest
+{
+    /** @test */
+    public function a_user_can_try_a_pin()
+    {
+        $provider = $this->getProvider();
+        $provider->tryIt('12345', 'my comment');
+
+        $request = [
+            'pin_id'  => '12345',
+            'details' => 'my comment',
+        ];
+        $this->assertWasPostRequest(UrlBuilder::RESOURCE_TRY_PIN_CREATE, $request);
+    }
+
+    /** @test */
+    public function a_user_can_edit_try_it_of_the_pin()
+    {
+        $provider = $this->getProvider();
+        $provider->editTryIt('12345', '56789', 'my comment');
+
+        $request = [
+            'pin_id'              => '12345',
+            'details'             => 'my comment',
+            'user_did_it_data_id' => '56789',
+        ];
+        $this->assertWasPostRequest(UrlBuilder::RESOURCE_TRY_PIN_EDIT, $request);
+    }
+
+    /** @test */
+    public function it_can_fetch_users_who_have_tried_a_pin()
+    {
+        $provider = $this->getProvider();
+        $this->pinterestShouldReturn(['aggregated_pin_data' => ['id' => '56789']]);
+
+        $provider->tried('12345')->toArray();
+
+        $request = [
+            'field_set_key'    => 'did_it',
+            'show_did_it_feed' => true,
+            'aggregated_pin_data_id' => '56789'
+        ];
+        $this->assertWasGetRequest(UrlBuilder::RESOURCE_ACTIVITY, $request);
+    }
+}
