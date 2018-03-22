207e355e4c93e9a7b6316d76680fdc6f0af15234
Sergey Zhuk <seregazhuk88@gmail.com>
2018-03-22 14:03:17 +0000

    Apply fixes from StyleCI

--- src/Api/Providers/Comments.php
+++ src/Api/Providers/Comments.php
@@ -26,8 +26,6 @@ class Comments extends Provider
      */
     public function create($pinId, $text)
     {
-
-
         $requestOptions = [
             'objectId' => $this->getAggregatedPinId($pinId),
             'pinId'    => $pinId,
